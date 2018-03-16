1495e204cc81ea4d55b6de0f77da9b58bc62d62f
Ashish Kumar Mishra <ashish10677@gmail.com>
2018-03-16 11:52:49 +0530

    Add CV template
    CV template from styleshout added to the react app. Content is still static.
    
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div>
-        Footer
+      <footer>
+
+      <div className="row">
+
+         <div className="twelve columns">
+
+
+            <ul className="copyright">
+               <li>&copy; Ashish Kumar Mishra 2018</li>
+               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
+
+         </div>
+
+         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+
       </div>
+
+   </footer>
     );
   }
 }
