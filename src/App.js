363056f692325cccdfbd3db893dd717f5d14f138
Ashish Kumar Mishra <ashish10677@gmail.com>
2018-03-16 19:47:09 +0530

    Add the json file
    resumeData.json is added from which dynamic content is to be fetched. It contains various objects like about, testimonials, contact etc.
    
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
@@ -9,16 +10,44 @@ import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      resumeData: {}
+    };
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({resumeData:data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+  
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
-        <Header />
-        <About />
-        <Resume />
-        <Portfolio />
-        <Testimonials />
-        <Contact />
-        <Footer />
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
       </div>
     );
   }
